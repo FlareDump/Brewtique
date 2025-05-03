98d5a133aff7efc04fdc443f23ace03cb950f043
Flare <rayverdasalla@gmail.com>
2025-05-03 21:06:32 +0800

    fixed home routings

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,7 +8,10 @@
     <title>Document</title>
     @vite('resources/css/app.css')
 </head>
+
 <body class="bg-bgPrimary">
     <!--Mao Ning Landing Page nato gois-->
+    Home Page?
 </body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages/home');
 });
+
+Route::get('/home', [PageController::class, 'welcome']);
